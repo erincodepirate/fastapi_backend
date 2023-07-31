7c40d5ed71d842ef024f005b964717bcc620e25c
Erin Clark <erincodepirate@gmail.com>
2023-07-31 16:49:04 -0700

    Add sqlalchemy database example

--- product/main.py
+++ product/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
-from . import schemas
+import schemas
+import models
+from database import engine
 
 app = FastAPI()
+
+models.Base.metadata.create_all(engine)
 
 @app.post('/product')
 def add(request: schemas.Product):
---
+++ product/models.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+class Product(Base):
+    __tablename__ = 'products'
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(length=50))
+    description = Column(String(length=255))
+    price = Column(Integer)
