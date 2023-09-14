ffc1be10d0debdcf076fbce56643a7c4bf969f6f
Erin Clark <erincodepirate@gmail.com>
2023-09-14 16:19:12 -0700

    Add login method

--- product/routers/login.py
+++ product/routers/login.py
@@ -1,13 +1,27 @@
 from fastapi import APIRouter, Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordBearer
 from database import get_db
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session
+from secret_key import SECRET_KEY
+from datetime import datetime, timedelta
+from jose import jwt, JWTError
 import schemas
 import models
 
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 20
+
 router = APIRouter()
 pwd_context = CryptContext(schemes=["bcrypt"],deprecated="auto")
+oath2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
+def generate_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes = ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp":expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
 
 @router.post('/login')
 def login(request: schemas.Login, db: Session = Depends(get_db)):
@@ -15,10 +29,25 @@ def login(request: schemas.Login, db: Session = Depends(get_db)):
         models.Seller.username == request.username).first()
     if not seller:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Invalid User")
-    print(seller)
-    print('hi')
     if not pwd_context.verify(request.password, seller.password):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Invalid Password")
 
-    #generate JWT token
-    return request
+    access_token = generate_token(data = {"sub":seller.username})
+    return {"access_token":access_token, "token_type":"bearer"}
+
+
+def get_current_user(token: str = Depends(oath2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid auth credentials",
+        headers={'WWW-Authenticate', 'Bearer'}
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get('sub')
+        if username is None:
+            raise credentials_exception
+        token_data = schemas.TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+
